316c6fc1ceeea544eeb3a33b66059bd68d671eaf
itachilee <3050155685@qq.com>
2019-09-02 12:54:34 +0800

    show groups\' create info

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_031432) do
+ActiveRecord::Schema.define(version: 2019_09_02_044419) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.text "descriptioon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
