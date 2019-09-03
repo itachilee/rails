28b729bdd80d3743998b437936d424f759bd18a2
itachilee <3050155685@qq.com>
2019-09-03 22:14:00 +0800

    when created a group ,join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 		@group = Group.new(group_params)
 		@group.user = current_user
 		if @group.save
+			current_user.join!(@group)
 			redirect_to groups_path
 		else
 			render :new
@@ -37,13 +38,38 @@ class GroupsController < ApplicationController
 		#flash[:alert] = "Group deleted"
 		redirect_to groups_path,notice:"Deleted Success"
 	end
-	private 
+	# 加入板块
+	def join
+		@group = Group.find(params[:id])
+		if !current_user.is_member_of?(@group)
+			current_user.join!(@group)
+			flash[:notice] = "加入本论坛版成功"
+		else
+			flash[:warning] = "你已经是本论坛的成员了"
+		end
+		redirect_to group_path(@group)
+	end
+	# 退出板块
+	def quit
+		@group = Group.find(params[:id])
+		if current_user.is_member_of?(@group)
+			current_user.quit!(@group)
+			flash[:alert] = "你已经退出本论坛块"
+		else
+			flash[:warning] = "你还没有加入本论坛你oto"
+		end
+		redirect_to group_path(@group)
+	end
+	#私有块
+	private
+        #权限验证	
 	def find_group_and_check_permission
 		@group = Group.find(params[:id])
 		if current_user != @group.user
 			redirect root_path,alert:"you have no permission."
 		end
 	end
+	# 权限指定
 	def group_params
 		params.require(:group).permit(:title,:descriptioon)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+	# 拦截
 	devise_for :users
+	# 分组资源
 	resources :groups do
+		member do
+			post :join
+			post :quit
+		end
 		resources :posts
 	end
 #	root 'welcome#index'	
+	# posts 文章资源
+	resources :post do 
+		# 热门
+		collection do
+			get :hot
+		end
+	end
+	# 首页路径设定
 	root 'groups#index'	      
 	
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
